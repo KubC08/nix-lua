a535f9efd2ab905e0e8b0021b25f226306beaeb9
KubC08 <54985640+KubC08@users.noreply.github.com>
2024-03-27 16:21:02 +0100

    Added more platforms

--- nix-lua/premake5.lua
+++ nix-lua/premake5.lua
@@ -9,12 +9,30 @@ OBJ_DIR = GetFromRoot("obj/%{cfg.buildcfg}/%{cfg.platform}/%{prj.name}")
 
 workspace("Nix.Lua")
     configurations({ "Debug", "Release" })
-    platforms({ "Linux" })
+    platforms({ "Linux", "Windows", "Mac", "MacARM", "Mac64" })
     architecture("x86_64")
 
     filter({ "platforms:Linux" })
         system("linux")
         defines({"_UNIX"})
+
+    filter({ "platforms:Windows" })
+        system("windows")
+        defines({"_WIN"})
+
+    filter({ "platforms:Mac" })
+        system("macosx")
+        architecture("universal")
+        defines({"_MAC"})
+
+    filter({ "platforms:MacARM" })
+        system("macosx")
+        architecture("ARM64")
+        defines({"_MAC", "_ARM64"})
+
+    filter({ "platforms:Mac64" })
+        system("macosx")
+        defines({"_MAC", "_MAC_64"})
 
     project("Nix.Lua")
         kind("SharedLib")
